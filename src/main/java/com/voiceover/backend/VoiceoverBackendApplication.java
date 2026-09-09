b958f949f2f7fe6de0cf2325dd3ddc39d57d6427
BTLoc2905 <thanhloc29052006@gmail.com>
2026-09-09 11:24:17 +0700

    chore: setup spring boot 3-tier folder structure

---
+++ src/main/java/com/voiceover/backend/VoiceoverBackendApplication.java
@@ -0,0 +1,13 @@
+package com.voiceover.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoiceoverBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoiceoverBackendApplication.class, args);
+	}
+
+}
